f21e02c62a6d75f1b396cb064f5ee8c93176ac02
PhRaz <philippe.razavet@gmail.com>
2018-01-26 00:06:33 +0100

    wip

--- app.js
+++ app.js
@@ -7,34 +7,44 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var path = require('path');
 
-app.get('/', function(req, res, next) {
+app.get('/', function (req, res, next) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 /*
  * connexion d'un nouveau client
  */
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('a user connected');
-    socket.on('disconnect', function(){
+
+    socket.emit('demande_pseudo');
+
+    socket.on('disconnect', function () {
         console.log('user disconnected');
     });
-    socket.on('message', function(msg) {
-       console.log('message: ' + msg);
+
+    socket.on('pseudo', function (msg) {
+        console.log('pseudo: ' + msg);
+        socket.emit('message', msg + ' a rejoint la conversation')
+    });
+
+    socket.on('message', function (msg) {
+        console.log('message: ' + msg);
+        socket.broadcast.emit('message', msg);
     });
 });
 
 /*
  * redirection si url non trouvée
  */
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.redirect('/');
 });
 
 /*
  * démarrage du serveur
  */
-server.listen(8080, function() {
+server.listen(8080, function () {
     console.log('listening on *:8080');
 });
 
